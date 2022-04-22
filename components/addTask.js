f5586e686766008ceb742e804c1c854a288081dd
Cristian <veracristian89@gmail.com>
2022-04-22 10:00:07 -0300

    fin del curso

--- components/addTask.js
+++ components/addTask.js
@@ -3,54 +3,61 @@ import deleteIcon from './deleteIcon.js';
 import { displayTasks } from './readTasks.js';
 
 export const addTask = (evento) => {
-    evento.preventDefault();
+  evento.preventDefault();
 
-    const list = document.querySelector('[data-list]');
-    const input = document.querySelector('[data-form-input]');
-    const calendar = document.querySelector("[data-form-date]");
+  const list = document.querySelector('[data-list]');
+  const input = document.querySelector('[data-form-input]');
+  const calendar = document.querySelector('[data-form-date]');
 
-    const value = input.value;
-    const date = calendar.value;
-    const dateFormat = moment(date).format("DD/MM/YYYY");
+  const value = input.value;
+  const date = calendar.value;
+  const dateFormat = moment(date).format('DD/MM/YYYY');
 
-    if (value == "" || date == ""){
-      return;
-    }
-    
-    input.value = '';
-    calendar.value= '';
+  if (value === '' || date === '') {
+    return;
+  }
 
-    const taskObj = {
-        value,
-        dateFormat
-    };
+  input.value = '';
+  calendar.value = '';
 
-    list.innerHTML = ""
+  const complete = false;
 
-    const taskList = JSON.parse(localStorage.getItem("tasks")) || [];
-    taskList.push(taskObj);
-    localStorage.setItem("tasks", JSON.stringify(taskList));
-
-    displayTasks();
-
+  const taskObj = {
+    value,
+    dateFormat,
+    complete,
+    id: uuid.v4(),
   };
-  
-  export const createTask = ({value, dateFormat}) => {
-    const task = document.createElement('li');
-          task.classList.add('card');
 
-    const taskContent = document.createElement('div');
-    
-    const titleTask = document.createElement('span');
-          titleTask.classList.add('task');
-          titleTask.innerText = value;
-          taskContent.appendChild(checkComplete());
-          taskContent.appendChild(titleTask);
-    // task.innerHTML = content;
-    const dateElemente = document.createElement("span");
-          dateElemente.innerHTML = dateFormat;
-          task.appendChild(taskContent);
-          task.appendChild(dateElemente);
-          task.appendChild(deleteIcon());
-    return task;
-  };
+  list.innerHTML = '';
+
+  const taskList = JSON.parse(localStorage.getItem('tasks')) || [];
+  taskList.push(taskObj);
+  localStorage.setItem('tasks', JSON.stringify(taskList));
+
+  displayTasks();
+};
+
+export const createTask = ({ value, dateFormat, complete, id }) => {
+  const task = document.createElement('li');
+  task.classList.add('card');
+
+  const taskContent = document.createElement('div');
+
+  const check = checkComplete(id);
+
+  if (complete) {
+    check.classList.toggle('fas');
+    check.classList.toggle('completeIcon');
+    check.classList.toggle('far');
+  }
+  const titleTask = document.createElement('span');
+  titleTask.classList.add('task');
+  titleTask.innerText = value;
+  taskContent.appendChild(check);
+  taskContent.appendChild(titleTask);
+
+  task.appendChild(taskContent);
+  task.appendChild(deleteIcon(id));
+  return task;
+};
--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-import { addTask } from "./components/addTask.js";
-import { displayTasks } from "./components/readTasks.js";
+import { addTask } from './components/addTask.js';
+import { displayTasks } from './components/readTasks.js';
 
 const btn = document.querySelector('[data-form-btn]');
 
